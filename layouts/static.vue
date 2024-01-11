21ebf3540353e7497ff9d551acdcd230f178bc42
xvvhang <xvvhang@gmail.com>
2024-01-11 23:49:21 +0800

    feat: add layout for static pages
    Changes to be committed:
      layouts/static.vue

---
+++ layouts/static.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto max-w-screen-md">
+    <a href="https://www.ycombinator.com/" target="_blank" class="flex align-center">
+      <span class="w-24 h-24 bg-orange-500 text-center text-5xl leading-loose text-white">Y</span>
+      <span class="px-4 text-5xl leading-loose font-medium text-orange-500">Combinator</span>
+    </a>
+    <slot />
+    <p class="border-t-4 border-t-orange-500"></p>
+  </div>
+</template>
