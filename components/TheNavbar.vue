2d8cf5b0a2eccde9af673390303b1283cff55fe9
xvvhang <xvvhang@gmail.com>
2024-01-14 00:01:02 +0800

    feat: redirect root path to new page
    Changes to be committed:
      modified:   components/TheNavbar.vue
      new file:   middleware/redirect.global.ts

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav class="bg-orange-500 px-2 py-1 flex justify-between items-center text-sm">
+  <nav class="bg-orange-500 px-2 py-1 flex items-center text-sm">
     <ul class="flex items-center gap-x-2">
       <li><a href="/" class="font-bold">Hacker News</a></li>
-      <li v-for="item in NavItems" :key="item.name" class="hover:text-white">
+      <li v-for="item in NavItems" :key="item.name" :class="{ 'text-white': active === item.name }" class="hover:text-white">
         <a :href="item.link">{{ item.name }}</a>
       </li>
-    </ul>
-    <ul class="flex items-center gap-x-2">
-      <li class="hover:text-white"><a href="/user">USER</a></li>
-      <li v-if="!isLogin" class="hover:text-white"><a href="/login">login</a></li>
-      <li v-if="isLogin" class="hover:text-white"><a href="/logout">logout</a></li>
     </ul>
   </nav>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const NavItems = [
   { link: '/new', name: 'new' },
   { link: '/top', name: 'top' },
   { link: '/best', name: 'best' },
   { link: '/ask', name: 'ask' },
   { link: '/show', name: 'show' },
-  { link: '/job', name: 'jobs' },
+  { link: '/job', name: 'job' },
 ]
 
-const isLogin = false
+const route = useRoute()
+const active = route.params.list
 </script>
-
-<style></style>
